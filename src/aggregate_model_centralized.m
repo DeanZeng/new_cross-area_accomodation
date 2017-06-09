eedf64494aa557f8b41dd2106de81beff601c13f
DeanZeng <zfangdi@gmail.com>
2017-06-09 15:42:48 +0800

    aggregate model in centralized mode

--- src/aggregate_model_centralized.m
+++ src/aggregate_model_centralized.m
@@ -10,154 +10,173 @@ elseif nargin == 3
 end
 
 %% input data
-T           = in.T;                 %% time horizons
-% Nunit       = in.Nunit;             %% number of units
-Ntype       = in.Ntype;             %% number of unit type
-Ntie        = in.Ntie;              %% number of tie lines
-%%------------------------ thermal unit ---------------------------------
-Pmax        = in.Pmax;              %% unit maximum output        1xNtype
-Pmin        = in.Pmin;              %% unit minimum output        1xNtype       
-Rampup      = in.Rampup;            %% unit ramping up rates      1xNtype
-Rampdown    = in.Rampdown;          %% unit ramping down rates    1xNtype
-Minup       = in.Minup;             %% unit minimun up time       1xNtype
-Mindown     = in.Mindown;           %% unit minimum dowm time     1xNtype
-S_t0        = in.S_t0;              %% initial number being on at t=0      1xNtype
-Pagg_t0     = in.Pagg_t0;           %% initial output at t=0      1xNtype
-TY_t0       = in.TY_t0;             %% length of time Yg has to be 0 at the beginning       1xNtype
-TZ_t0       = in.TY_t0;             %% length of time Zg has to be 0 at the beginning       1xNtype
-Ng          = in.Ng;                %% number per unit type       1xNtype
-% typeID      = in.typeID;            %% type ID                    1xNunit        
-%%--------------------------  wind and PV ---------------------------------
-Windmax     = in.Windmax;           %% theory output of wind power
-PVmax       = in.PVmax;             %% theory output of PV
-%%--------------------------  tie lines -----------------------------------
-Tieline     = in.Tieline;           %% tie lines
-Ftie0       = in.Ftie0;             %% fixed power flow
-Etie        = in.Etie;              %% exchage energy each day
-TDstart     = in.TDstart;
-%%---------------------------- system -------------------------------------
-Demand      = in.Demand;            %% demand
-ReserveUp   = in.ReserveUp;         %% up reserve
-ReserveDn   = in.ReserveDn;         %% dowen reserve
-%%---------------------------- ADMM ---------------------------------------
-% Ftie_val    = in.Ftie_val;          %% exchange information of tie line power flow 
-% lamda       = in.lamda;             %% multiplers
-% Rho         = in.Rho;               %% coefficient of quadratic term
-%% variable
-%%--------------------------- wind power & PV -----------------------------
-Pwind=sdpvar(T,1,'full');    %% output of wind power 
-Ppv  =sdpvar(T,1,'full');    %% output of PV 
+% T           = in.T;                 %% time horizons
+% % Nunit       = in.Nunit;             %% number of units
+% Ntype       = in.Ntype;             %% number of unit type
+% Ntie        = in.Ntie;              %% number of tie lines
+% %%------------------------ thermal unit ---------------------------------
+% Pmax        = in.Pmax;              %% unit maximum output        1xNtype
+% Pmin        = in.Pmin;              %% unit minimum output        1xNtype       
+% Rampup      = in.Rampup;            %% unit ramping up rates      1xNtype
+% Rampdown    = in.Rampdown;          %% unit ramping down rates    1xNtype
+% Minup       = in.Minup;             %% unit minimun up time       1xNtype
+% Mindown     = in.Mindown;           %% unit minimum dowm time     1xNtype
+% S_t0        = in.S_t0;              %% initial number being on at t=0      1xNtype
+% Pagg_t0     = in.Pagg_t0;           %% initial output at t=0      1xNtype
+% TY_t0       = in.TY_t0;             %% length of time Yg has to be 0 at the beginning       1xNtype
+% TZ_t0       = in.TY_t0;             %% length of time Zg has to be 0 at the beginning       1xNtype
+% Ng          = in.Ng;                %% number per unit type       1xNtype
+% % typeID      = in.typeID;            %% type ID                    1xNunit        
+% %%--------------------------  wind and PV ---------------------------------
+% Windmax     = in.Windmax;           %% theory output of wind power
+% PVmax       = in.PVmax;             %% theory output of PV
+% %%--------------------------  tie lines -----------------------------------
+% Tieline     = in.Tieline;           %% tie lines
+% Ftie0       = in.Ftie0;             %% fixed power flow
+% Etie        = in.Etie;              %% exchage energy each day
+% TDstart     = in.TDstart;
+% %%---------------------------- system -------------------------------------
+% Demand      = in.Demand;            %% demand
+% ReserveUp   = in.ReserveUp;         %% up reserve
+% ReserveDn   = in.ReserveDn;         %% dowen reserve
+% %%---------------------------- ADMM ---------------------------------------
+% % Ftie_val    = in.Ftie_val;          %% exchange information of tie line power flow 
+% % lamda       = in.lamda;             %% multiplers
+% % Rho         = in.Rho;               %% coefficient of quadratic term
+T  = in(1).T;
+TD = in(1).TD;
+for a=1:A
+    %% variable
+    %%--------------------------- wind power & PV -----------------------------
+    var(a).Pwind=sdpvar(T,1,'full');    %% output of wind power 
+    var(a).Ppv  =sdpvar(T,1,'full');    %% output of PV 
 
-%%--------------------------- thermal unit --------------------------------
-Pagg     = sdpvar(T,Ntype,'full');   %% output of thermal unit
-S        = intvar(T,Ntype,'full');   %% on_off status;
-Y        = binvar(T,Ntype,'full');   %% start up indicator
-Z        = binvar(T,Ntype,'full');   %% shut down indicator
-%%---------------------------- tie lines ----------------------------------
-Ftie = sdpvar(T,Ntie,'full');        %% tie-line power flow
-%% initial assign
-if UseX0
-    assign(Pwind, x0.Pwind);
-    assign(Ppv, x0.Ppv);
-    assign(Pagg, x0.Pagg);
-    assign(S, x0.S);
-    assign(Y, x0.Y);
-    assign(Z, x0.Z);
-    assign(Ftie, x0.Ftie);
-end 
+    %%--------------------------- thermal unit --------------------------------
+    var(a).Pagg     = sdpvar(T,Ntype,'full');   %% output of thermal unit
+    var(a).S        = intvar(T,Ntype,'full');   %% on_off status;
+    var(a).Y        = binvar(T,Ntype,'full');   %% start up indicator
+    var(a).Z        = binvar(T,Ntype,'full');   %% shut down indicator
+    %%---------------------------- tie lines ----------------------------------
+    var(a).Ftie = sdpvar(T,Ntie,'full');        %% tie-line power flow
+    %% initial assign
+    if UseX0
+        assign(var(a).Pwind, x0(a).Pwind);
+        assign(var(a).Ppv, x0(a).Ppv);
+        assign(var(a).Pagg, x0(a).Pagg);
+        assign(var(a).S, x0(a).S);
+        assign(var(a).Y, x0(a).Y);
+        assign(var(a).Z, x0(a).Z);
+        assign(var(a).Ftie, x0(a).Ftie);
+    end 
+end
 %% constraints
 Constraint=[];
-%--------------------- thermal unit constraints ------------------------
-% binary & interger variable logic
-Constraint=[Constraint,(-Ng.*Z(1,:) <= S(1,:)-S_t0 <= Ng.*Y(1,:)):'logical_1t0'];
-for t=2:T
-    Constraint=[Constraint,(-Ng.*Z(t,:) <= S(t,:)-S(t-1,:) <= Ng.*Y(t,:)):'logical_1'];
-end
-Constraint=[Constraint,(Y+Z <= ones(T,Ntype)):'logical_2'];
-% output limit
-for t = 1:T
-   Constraint = [Constraint, (S(t,:).*Pmin <=...
-       Pagg(t,:) <= S(t,:).*Pmax):'output limit'];
-   Constraint = [Constraint, (zeros(1,Ntype) <=...
-       S(t,:) <= Ng):'number limit'];
-end
-
-% minimum up/down time
-for g=1:Ntype
-    for t=1:TY_t0
-        Constraint = [Constraint,(Y(t,g) == 0 ):'initial Y'];
+for a=1:A
+    %--------------------- thermal unit constraints ------------------------
+    % binary & interger variable logic
+    Constraint=[Constraint,(-in(a).Ng.*var(a).Z(1,:) <= var(a).S(1,:)-in(a).S_t0 <= in(a).Ng.*var(a).Y(1,:)):'logical_1t0'];
+    for t=2:T
+        Constraint=[Constraint,(-in(a).Ng.*var(a).Z(t,:) <= var(a).S(t,:)-var(a).S(t-1,:) <= in(a).Ng.*var(a).Y(t,:)):'logical_1'];
     end
-    for t=1:TZ_t0
-        Constraint = [Constraint,(Z(t,g) == 0 ):'initial Z'];
+    Constraint=[Constraint,(in(a).Y + in(a).Z <= ones(T,in(a).Ntype)):'logical_2'];
+    % output limit
+    for t = 1:T
+       Constraint = [Constraint, (var(a).S(t,:).*in(a).Pmin <=...
+           var(a).Pagg(t,:) <= var(a).S(t,:).*in(a).Pmax):'output limit'];
+       Constraint = [Constraint, (zeros(1,in(a).Ntype) <=...
+           var(a).S(t,:) <= in(a).Ng):'number limit'];
     end
-    for t = TY_t0+1:T
-        tt=max(1,t-Minup(g)+1);
-        Constraint = [Constraint, (sum(Y(tt:t,g))...
-            <= 1):'min_up'];
-    end
-    for t = TZ_t0+1:T
-        tt=max(1,t-Mindown(g)+1);
-        Constraint = [Constraint, (sum(Z(tt:t,g))...
-            <= 1):'min_down'];
-    end  
-end
-% ramping up/down limit
-Constraint=[Constraint,(-Rampdown.*S(1,:) <= Pagg(1,:)-Pagg_t0...
-        <= Rampup.*S(1,:)):'ramp0'];
-for t=2:T
-    Constraint=[Constraint,(-Rampdown.*S(t,:) <= Pagg(t,:)-Pagg(t-1,:)...
-        <= Rampup.*S(t,:)):'ramp'];
-end
 
-%------------------------------- wind power & PV ----------------------
-Constraint=[Constraint,(zeros(T,1) <= Pwind <= Windmax):'wind power output limit'];
-Constraint=[Constraint,(zeros(T,1) <= Ppv   <= PVmax):'wind power output limit'];
-
-%------------------------------- Tie-line -----------------------------
-for line=1:Ntie
-    if Tieline(line,2)==1
-        Constraint=[Constraint,(Ftie(:,line)==Ftie0(:,line)):'tie typeI'];
-    elseif Tieline(line,2)==2
-        for t=1:T
-%             Constraint=[Constraint,(-Tieline(line,3)<=...
-%                 Ftie(t,line)<=Tieline(line,3)):'tie typeII'];
-            Constraint=[Constraint,(0 <= Ftie(t,line) <= 0 ):'tie typeII'];
+    % minimum up/down time
+    for g=1:in(a).Ntype
+        for t=1:in(a).TY_t0
+            Constraint = [Constraint,(var(a).Y(t,g) == 0 ):'initial Y'];
         end
-    elseif Tieline(line,2)==3
-        for t=1:T
-            Constraint=[Constraint,(-Tieline(line,3)<=...
-                Ftie(t,line)<=Tieline(line,3)):'tie typeIII 1'];
+        for t=1:in(a).TZ_t0
+            Constraint = [Constraint,(var(a).Z(t,g) == 0 ):'initial Z'];
         end
-        for td=1:TD
-           Constraint=[Constraint,(sum(Ftie(TDstart(td):TDstart(td+1),line))...
-               ==Etie(td,line)):'tie typeIII 2'];
+        for t = in(a).TY_t0+1:T
+            tt=max(1,t-in(a).Minup(g)+1);
+            Constraint = [Constraint, (sum(var(a).Y(tt:t,g))...
+                <= 1):'min_up'];
+        end
+        for t = in(a).TZ_t0+1:T
+            tt=max(1,t-in(a)Mindown(g)+1);
+            Constraint = [Constraint, (sum(var(a).Z(tt:t,g))...
+                <= 1):'min_down'];
+        end  
+    end
+    % ramping up/down limit
+    Constraint=[Constraint,(-in(a).Rampdown.*var(a).S(1,:) <= var(a).Pagg(1,:)-in(a).Pagg_t0...
+            <= in(a).Rampup.*var(a).S(1,:)):'ramp0'];
+    for t=2:T
+        Constraint=[Constraint,(-in(a).Rampdown.*var(a).S(t,:) <= var(a).Pagg(t,:)-var(a).Pagg(t-1,:)...
+            <= in(a).Rampup.*var(a).S(t,:)):'ramp'];
+    end
+
+    %------------------------------- wind power & PV ----------------------
+    Constraint=[Constraint,(zeros(T,1) <= var(a).Pwind <= in(a).Windmax):'wind power output limit'];
+    Constraint=[Constraint,(zeros(T,1) <= var(a).Ppv   <= in(a).PVmax):'wind power output limit'];
+
+    %------------------------------- Tie-line -----------------------------
+    for line=1:in(a).Ntie
+        if in(a).Tieline(line,2)==1
+            Constraint=[Constraint,(var(a).Ftie(:,line)==in(a).Ftie0(:,line)):'tie typeI'];
+        elseif in(a).Tieline(line,2)==2
+            for t=1:T
+                Constraint=[Constraint,(-in(a).Tieline(line,3)<=...
+                    var(a).Ftie(t,line)<= in(a).Tieline(line,3)):'tie typeII'];
+%                 Constraint=[Constraint,(0 <= var(a).Ftie(t,line) <= 0 ):'tie typeII'];
+            end
+        elseif in(a).Tieline(line,2)==3
+            for t=1:T
+                Constraint=[Constraint,(-in(a).Tieline(line,3)<=...
+                    var(a).Ftie(t,line)<= in(a).Tieline(line,3)):'tie typeIII 1'];
+            end
+            for td=1:TD
+               Constraint=[Constraint,(sum(var(a).Ftie(TDstart(td):TDstart(td+1),line))...
+                   == in(a).Etie(td,line)):'tie typeIII 2'];
+            end
+        end
+    end
+    %------------------------------- power balance ------------------------
+    for t=1:T
+        Constraint=[Constraint,(sum(var(a).Pagg(t,:))+var(a).Pwind(t)+var(a).Ppv(t)...
+            == in(a).Demand(t)+sum(var(a).Ftie(t,:))):'power balance'];
+    end
+    %------------------------------- spinning reserve ---------------------
+    % for t=1:T
+    %     Constraint=[Constraint,(sum(onoff(t,:).*Pmax)+Windmax(t)+PVmax(t)...
+    %         +sum(Tieline(:,3))>=Demand(t)+ReserveUp(t)):'up reserve'];
+    %     Constraint=[Constraint,(sum(onoff(t,:).*Pmin)-sum(Tieline(:,3))...
+    %         <=Demand(t)-ReserveDn(t)):'down reserve'];
+    % end
+    % for t=1:T
+    %     Constraint=[Constraint,(sum(onoff(t,:).*Pmax - Pthermal(t,:)) >= ReserveUp(t)):'up reserve'];
+    %     Constraint=[Constraint,(sum(onoff(t,:).*Pmin - Pthermal(t,:)) <= -ReserveDn(t)):'down reserve'];
+    % end
+end
+for a=1:A
+    for b=a+1:A
+        for la=1:in(a).Ntie
+            for lb=1:in(b).Ntie
+                if (in(a).Tieline(la,1)==b)&&(in(b).Tieline(lb,1)==a)
+                     Constraint = [Constraint,(var(a).Ftie(:,la) == var(b).Ftie(:,lb)):'concensus'];
+                end
+            end
         end
     end
 end
-%------------------------------- power balance ------------------------
-for t=1:T
-    Constraint=[Constraint,(sum(Pagg(t,:))+Pwind(t)+Ppv(t)...
-        ==Demand(t)+sum(Ftie(t,:))):'power balance'];
-end
-%------------------------------- spinning reserve ---------------------
-% for t=1:T
-%     Constraint=[Constraint,(sum(onoff(t,:).*Pmax)+Windmax(t)+PVmax(t)...
-%         +sum(Tieline(:,3))>=Demand(t)+ReserveUp(t)):'up reserve'];
-%     Constraint=[Constraint,(sum(onoff(t,:).*Pmin)-sum(Tieline(:,3))...
-%         <=Demand(t)-ReserveDn(t)):'down reserve'];
-% end
-% for t=1:T
-%     Constraint=[Constraint,(sum(onoff(t,:).*Pmax - Pthermal(t,:)) >= ReserveUp(t)):'up reserve'];
-%     Constraint=[Constraint,(sum(onoff(t,:).*Pmin - Pthermal(t,:)) <= -ReserveDn(t)):'down reserve'];
-% end
-
 %% objective
-minLang= -sum(Pwind)-sum(Ppv);
+minLang=0;
+for a=1:A
+minLang= -sum(var(a).Pwind)-sum(var(a).Ppv);
 %     for la=1:Ntie
 %         minLang = minLang +...
 %             lamda(:,la)'*(Ftie(:,la)-Ftie_val(:,la))+...
 %             Rho/2*(Ftie(:,la)-Ftie_val(:,la))'*(Ftie(:,la)-Ftie_val(:,la));                
 %     end
+end
 %% solution
 tic
 Ops = sdpsettings('solver','gurobi','usex0',1,'verbose',1,'showprogress',0);
@@ -175,7 +194,7 @@ end
 toc
 %% read values of variables
 %%--------------------------- wind power & PV -----------------------------
-out.Pwind = value(Pwind);    %% output of wind power 
+out.Pwind = value(var(a).Pwind);    %% output of wind power 
 out.Ppv   = value(Ppv);      %% output of PV 
 %%--------------------------- thermal unit --------------------------------
 out.Pagg  = value(Pagg);
